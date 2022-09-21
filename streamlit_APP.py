7e134177f3c8700bc5f79d7d1c16ce44e1f6628a
YS565 <gx_9900_nt_002@yahoo.co.jp>
2022-09-22 01:10:10 +0900

    1st commit

---
+++ streamlit_APP.py
@@ -0,0 +1,13 @@
+import streamlit as 
+from PIL import Image
+
+st.title('アプリ')
+
+uploaded_file = st.file_uploader('CHoose an image...',type='jpg',accept_multiple_files=True)
+
+if uploaded_file is not None:
+    for i in uploaded_file:
+        if st.checkbox(i.name):
+            img = Image.open(i)
+            # st.image(img,caption='Uploaded Image.',use_column_width=True)
+            st.image(img,caption=i.name,use_column_width=True)
