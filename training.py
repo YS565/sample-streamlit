cbe1353993e8cbb2abca4d8064b5edc41cb3e528
YS565 <114093714+YS565@users.noreply.github.com>
2022-09-28 12:57:36 +0900

    2nd commit

--- training.py
+++ training.py
@@ -1,5 +1,29 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
 import streamlit as st
 from PIL import Image
 
-st.title('Training')
+
+# In[7]:
+
+
+#ワイド表示
+st.set_page_config(layout="wide")
+
+#セレクトボックスのリストを作成
+page_list = ["page1","page2"]
+
+#サイドバーのセレクトボックスを配置
+selector=st.sidebar.selectbox( "ページ選択",page_list)
+
+if selector=="page1":
+    if st.button('ページ1ボタン'):
+        st.title("ページ1のタイトル")
+elif selector=="page2":
+    if st.button('ページ2ボタン'):
+        st.title("ページ2のタイトル")
 
