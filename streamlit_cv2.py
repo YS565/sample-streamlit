5179d445704df50419889a1aec529e46b0a5640e
YS565 <114093714+YS565@users.noreply.github.com>
2022-10-05 12:03:52 +0900

    Add files via upload

--- streamlit_cv2.py
+++ streamlit_cv2.py
@@ -12,7 +12,7 @@ st.write("Hello, world")
 
 webrtc_streamer(
     key="example",
-    video_frame_callback=callback,
+    # video_frame_callback=callback,
     rtc_configuration={  # この設定を足す
         "iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]
     }
